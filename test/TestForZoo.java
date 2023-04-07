1b4ab6f9a1798c771982767a3896b48d3d9d30eb
coldvl <volodymyr.holodnik@gmail.com>
2023-04-07 17:53:57 +0300

    initial commmit

---
+++ test/TestForZoo.java
@@ -0,0 +1,20 @@
+import java.sql.Time;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+import ua.lviv.iot.algo.part1.lab2.Zoo;
+
+
+
+public class TestForZoo 
+    extends Zoo
+{
+    Zoo zoo = new Zoo("Friendly Farms", "Canada", 100, 10, Time.valueOf("11:00:00"), Time.valueOf("16:00:00"), 20);
+
+    // test for method calculateCostPerMonth
+    @Test
+    public void testCalculateCostPerMonth() {
+        TestCase.assertEquals(600, zoo.calculateCostPerMonth());
+    }
+    
+}
