fbd40c862ff0fa975317e30be696936749e953ec
coldvl <volodymyr.holodnik@gmail.com>
2023-04-08 23:07:33 +0300

    lab3

--- test/TestForZoo.java
+++ test/TestForZoo.java
@@ -2,6 +2,7 @@ import java.sql.Time;
 
 import org.junit.Test;
 import junit.framework.TestCase;
+import ua.lviv.iot.algo.part1.lab2.AnimalHomeManager;
 import ua.lviv.iot.algo.part1.lab2.Zoo;
 
 
@@ -17,4 +18,25 @@ public class TestForZoo
         TestCase.assertEquals(600, zoo.calculateCostPerMonth());
     }
     
+    //test getters
+    @Test
+    public void testGetters() {
+        TestCase.assertEquals("Friendly Farms", zoo.getName());
+        TestCase.assertEquals(100.0, zoo.getArea());
+        TestCase.assertEquals("Canada", zoo.getLocation());
+        TestCase.assertEquals(20, zoo.getCostPerDay());
+        TestCase.assertEquals(Time.valueOf("11:00:00"), zoo.getOpeningTime());
+        TestCase.assertEquals(Time.valueOf("16:00:00"), zoo.getClosingTime());
+        
+    }
+
+    @Test
+    public void testAddNewRegion() {
+        double initialArea = 1000;
+        double newArea = 500;
+        this.zoo.addNewRegion(newArea);
+        double expectedArea = initialArea + newArea;
+        double actualArea = animalHome.getArea();
+        assertEquals(expectedArea, actualArea, 0.01);
+    }
 }
